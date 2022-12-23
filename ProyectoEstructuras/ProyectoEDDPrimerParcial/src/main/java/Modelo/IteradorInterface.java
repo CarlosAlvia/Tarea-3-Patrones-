225a23cce3ba2f0396a2cb099c98174080162b3a
CarlosAlvia <cjalvia@espol.edu.ec>
2022-12-23 13:19:51 -0500

    repo

---
+++ ProyectoEstructuras/ProyectoEDDPrimerParcial/src/main/java/Modelo/IteradorInterface.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author jexa1
+ */
+public interface IteradorInterface<T>{
+       public T next();
+       public T previous();
+    }
